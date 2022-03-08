ff416aa2e8c74dd137b8544f2d790bdc9764a058
Q Adipo <quincedya@gmail.com>
2022-03-08 09:51:56 +0300

    Add name determining function

--- js/myscripts.js
+++ js/myscripts.js
@@ -2,6 +2,39 @@ function getInputValue(){
     var inputDate = document.getElementById("birthday").value;
     var inputGender = document.getElementById().value;
 }
-function confirmDate(){
+var dayofWeek = inputDate.getDay()
 
+if (inputGender == "Male"){
+    var nameGiven = '';
+    if(dayofWeek === 0){
+        nameGiven = "Kwasi";
+    } else if (dayofWeek === 1) {
+        nameGiven = "Kwadwo"
+    } else if (dayofWeek === 2) {
+        nameGiven = "Kwabena"
+    } else if (dayofWeek === 3) {
+        nameGiven = "Kwaku"
+    } else if (dayofWeek === 4) {
+        nameGiven = "Yaw"
+    } else if (dayofWeek === 5) {
+        nameGiven = "Kofi"
+    } else (dayofWeek === 1) {
+        nameGiven = "Kwame"
+    }
+} else {
+    if(dayofWeek === 0){
+        nameGiven = "Akosua";
+    } else if (dayofWeek === 1) {
+        nameGiven = "Adwoa"
+    } else if (dayofWeek === 2) {
+        nameGiven = "Abenaa"
+    } else if (dayofWeek === 3) {
+        nameGiven = "Akua"
+    } else if (dayofWeek === 4) {
+        nameGiven = "Yaa"
+    } else if (dayofWeek === 5) {
+        nameGiven = "Afua"
+    } else (dayofWeek === 1) {
+        nameGiven = "Ama"
+    }
 }
