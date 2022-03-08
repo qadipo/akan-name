bbfa92692955a8ad160bc3dffa6fb81e504a734b
Q Adipo <quincedya@gmail.com>
2022-03-08 09:53:36 +0300

    Enclose givenName as a function

--- js/myscripts.js
+++ js/myscripts.js
@@ -4,37 +4,39 @@ function getInputValue(){
 }
 var dayofWeek = inputDate.getDay()
 
-if (inputGender == "Male"){
-    var nameGiven = '';
-    if(dayofWeek === 0){
-        nameGiven = "Kwasi";
-    } else if (dayofWeek === 1) {
-        nameGiven = "Kwadwo"
-    } else if (dayofWeek === 2) {
-        nameGiven = "Kwabena"
-    } else if (dayofWeek === 3) {
-        nameGiven = "Kwaku"
-    } else if (dayofWeek === 4) {
-        nameGiven = "Yaw"
-    } else if (dayofWeek === 5) {
-        nameGiven = "Kofi"
-    } else (dayofWeek === 1) {
-        nameGiven = "Kwame"
-    }
-} else {
-    if(dayofWeek === 0){
-        nameGiven = "Akosua";
-    } else if (dayofWeek === 1) {
-        nameGiven = "Adwoa"
-    } else if (dayofWeek === 2) {
-        nameGiven = "Abenaa"
-    } else if (dayofWeek === 3) {
-        nameGiven = "Akua"
-    } else if (dayofWeek === 4) {
-        nameGiven = "Yaa"
-    } else if (dayofWeek === 5) {
-        nameGiven = "Afua"
-    } else (dayofWeek === 1) {
-        nameGiven = "Ama"
+function givenName(dayofWeek){
+    if (inputGender == "Male"){
+        var nameGiven = '';
+        if(dayofWeek === 0){
+            nameGiven = "Kwasi";
+        } else if (dayofWeek === 1) {
+            nameGiven = "Kwadwo"
+        } else if (dayofWeek === 2) {
+            nameGiven = "Kwabena"
+        } else if (dayofWeek === 3) {
+            nameGiven = "Kwaku"
+        } else if (dayofWeek === 4) {
+            nameGiven = "Yaw"
+        } else if (dayofWeek === 5) {
+            nameGiven = "Kofi"
+        } else (dayofWeek === 1) {
+            nameGiven = "Kwame"
+        }
+    } else {
+        if(dayofWeek === 0){
+            nameGiven = "Akosua";
+        } else if (dayofWeek === 1) {
+            nameGiven = "Adwoa"
+        } else if (dayofWeek === 2) {
+            nameGiven = "Abenaa"
+        } else if (dayofWeek === 3) {
+            nameGiven = "Akua"
+        } else if (dayofWeek === 4) {
+            nameGiven = "Yaa"
+        } else if (dayofWeek === 5) {
+            nameGiven = "Afua"
+        } else (dayofWeek === 1) {
+            nameGiven = "Ama"
+        }
     }
 }
