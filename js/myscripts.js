78207c244635d9eb65a725d95cdd0f66d4b8c09d
Q Adipo <quincedya@gmail.com>
2022-03-08 11:41:48 +0300

    Write validation functions and the dayofWeekFunction

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,7 +1,7 @@
 var inputDate = ''
 var inputGender = ''
 function getInputValue(){
-    inputDate = document.getElementById("birthday").value;
+    inputDate = document.getElementById("birthday").value.toString();
     // var inputGender = document.getElementsByName('gender');
     // var inputGender=''
     if(document.getElementById("male").checked){
@@ -14,9 +14,54 @@ function getInputValue(){
     }
 }
 
-var dayofWeek = inputDate.getDay();
-var ccDate = inputDate.slice()
+function genderValidation{
+    if (inputGender == ''){
+        alert('Please choose your gender then submit');
+    }
+}
 
+function dateValidation(inputDate){
+    var dayDate = inputDate.slice(0,2);
+    if(isNan(dayDate)){
+        alert("Date must be a number");  
+    } else if(parseInt(dayDate) <= 0 || parseInt(dayDate) > 31){
+        alert("Input Invalid");
+    } else {
+        return dayDate;
+    }
+}
+
+function monthValidation(month){
+    var month = inputDate.slice(3,5);
+    if (isNan(month)){
+        alert("Month must be a number")
+    } else if (parseInt(month) <= 0 || parseInt(month) > 12){
+        alert("Invalid month");
+    } else {
+        return month;
+    }
+}
+
+function yearValidation(year){
+    var year = inputDate.slice(6,10);
+    if (isNan(year)){
+        alert('Year must be a number');
+    } else if (year.length != 4){
+        alert('Year must have four digits');
+    } else {
+        return year;
+    }
+}
+
+// Get day of the week function
+function dayofWeekFunction(dayDate, month, year){
+    var CC = parseInt(year.slice(0,2));
+    var YY = parseInt(year.slice(2,4));
+    var MM = parseInt(month);
+    var DD = parseInt(dayDate);
+
+    var dayofWeek = ((((CC/4) - 2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD) % 7)
+}
 
 function givenName(dayofWeek, inputGender){
     if (inputGender == "Male"){
@@ -55,3 +100,4 @@ function givenName(dayofWeek, inputGender){
     }
     return nameGiven;
 }
+
